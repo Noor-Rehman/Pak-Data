7890578f0865a8767d29ab0647f572b51f5e9027
Noor Rehman <hello.noorrehman@gmail.com>
2025-11-09 21:21:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1478,4 +1478,5 @@ def generate_outlier_visualization():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
